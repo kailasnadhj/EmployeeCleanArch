72fca7786452e3284f163bc081531d88060719ea
kailasnadhj <kailas.j@experionglobal.com>
2025-01-17 10:22:21 +0530

    added a command and a query logic

---
+++ EmployeeCleanArch.Application/Features/Departments/Commands/AddNewDepartmentCommand.cs
@@ -0,0 +1,37 @@
+﻿using EmployeeCleanArch.Application.DTOs;
+using EmployeeCleanArch.Application.Interfaces.Repositories;
+using EmployeeCleanArch.Domain.Entities;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EmployeeCleanArch.Application.Features.Departments.Commands
+{
+    public record AddNewDepartmentCommand(CreateDepartmentDTO departmentDTO) : IRequest<Department>;
+
+    public class AddNewDepartmentCommandHandler : IRequestHandler<AddNewDepartmentCommand, Department>
+    {
+        private readonly IGenericRepository<Department> _repository;
+
+        public AddNewDepartmentCommandHandler(IGenericRepository<Department> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Department> Handle(AddNewDepartmentCommand request, CancellationToken cancellationToken)
+        {
+            var departmentEntity = new Department
+            {
+                DepartmentName = request.departmentDTO.DepartmentName,
+                MaxCapacity = request.departmentDTO.MaxCapacity,
+                Status = request.departmentDTO.Status,
+                Location = request.departmentDTO.Location,
+                CreatedBy = request.departmentDTO.CreatedBy,
+                CreatedDate = DateTime.Now
+            };
+
+            return await _repository.AddAsync(departmentEntity);
+        }
+    }
+}
---
+++ EmployeeCleanArch.Application/Features/Departments/Queries/GetAllDepartmentsQuery.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using EmployeeCleanArch.Domain.Entities;
+using EmployeeCleanArch.Application.Interfaces.Repositories;
+
+namespace EmployeeCleanArch.Application.Features.Departments.Queries
+{
+    public record GetAllDepartmentsQuery() : IRequest<IEnumerable<Department>>;
+
+    public class GetAllDepartmentsQueryHandler : IRequestHandler<GetAllDepartmentsQuery, IEnumerable<Department>>
+    {
+        private readonly IGenericRepository<Department> _repository;
+
+        public GetAllDepartmentsQueryHandler(IGenericRepository<Department> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<Department>> Handle(GetAllDepartmentsQuery request, CancellationToken cancellationToken)
+        {
+            return await _repository.GetAllAsync();
+        }
+    }
+}
